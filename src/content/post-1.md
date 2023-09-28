2897adba5d24c7f2d49a1fbcb2dd524c8d466484
selena <dmhebert@student.fullsail.edu>
2023-09-28 11:32:00 -0500

    init

---
+++ src/content/post-1.md
@@ -0,0 +1,6 @@
+---
+slug: "testing"
+date: "2022-11-24"
+title: "My first blog post"
+content: "This is my first post with a description and content testing."
+---
---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+export default function Index() {
+  return <div>Hello world</div>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as React from "react"
+
+
+// const About = () => {
+//   return ( 
+//     <div className="" >
+//       About page
+//     </div>
+//    );
+// }
+ 
+// export default About;
