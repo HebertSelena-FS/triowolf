c8f370c4ed2fede97bf4d68c7d4c3dc10444f9f3
selena <dmhebert@student.fullsail.edu>
2024-07-16 22:49:15 -0500

    added analytics from vercel

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Analytics>
+      <Router>
+        <App />
+      </Router>
+    </Analytics>
   </React.StrictMode>
 );
